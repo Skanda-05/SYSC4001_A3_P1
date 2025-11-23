ddbbe323e3ee923a10e544409f92317e76a9a364
skand <skanda.nagendra23@gmail.com>
2025-11-22 21:38:27 -0500

    added implementation for EP_RR (very similar to EP and RR individually, just now put together)

--- interrupts_101299202_101294988_EP_RR.cpp
+++ interrupts_101299202_101294988_EP_RR.cpp
@@ -8,16 +8,19 @@
  */
 
 #include<interrupts_101299202_101294988.hpp>
+#define TIME_QUANTUM 100
 
-void FCFS(std::vector<PCB> &ready_queue) {
+//Sorting the queue based on prioritiy.
+//Looks a lot like FCFS, but order is determined by priority value instead of arrival time. 
+void priority_sort(std::vector<PCB> &ready_queue) {
     std::sort( 
                 ready_queue.begin(),
                 ready_queue.end(),
                 []( const PCB &first, const PCB &second ){
-                    return (first.arrival_time > second.arrival_time); 
+                    return (first.priority > second.priority); 
                 } 
             );
-}
+}    
 
 std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std::vector<PCB> list_processes) {
 
@@ -29,6 +32,8 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
                                     //to make the code easier :).
 
     unsigned int current_time = 0;
+    unsigned int quantum_remaining = TIME_QUANTUM;
+    unsigned int cpu_time_since_last_io = 0;
     PCB running;
 
     //Initialize an empty running process
@@ -50,28 +55,107 @@ std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std
 
         //Population of ready queue is given to you as an example.
         //Go through the list of proceeses
-        for(auto &process : list_processes) {
+         for(auto &process : list_processes) {
             if(process.arrival_time == current_time) {//check if the AT = current time
                 //if so, assign memory and put the process into the ready queue
-                assign_memory(process);
+                if (assign_memory(process)) {
 
-                process.state = READY;  //Set the process state to READY
-                ready_queue.push_back(process); //Add the process to the ready queue
-                job_list.push_back(process); //Add it to the list of processes
+                    process.state = READY;  //Set the process state to READY
+                    ready_queue.push_back(process); //Add the process to the ready queue
+                    job_list.push_back(process); //Add it to the list of processes
 
-                execution_status += print_exec_status(current_time, process.PID, NEW, READY);
+                    execution_status += print_exec_status(current_time, process.PID, NEW, READY);
+                }
+                else {
+                    process.state = NEW; // process remains in NEW
+                    job_list.push_back(process); // will need to check again later. add it to job list again for nwo
+                    std::cout << "Process " << process.PID << " could not be assigned memory at time " << current_time << std::endl;
+                    
+                }
             }
         }
 
         ///////////////////////MANAGE WAIT QUEUE/////////////////////////
         //This mainly involves keeping track of how long a process must remain in the ready queue
-
+        for (auto iter = wait_queue.begin(); iter != wait_queue.end();) {
+            iter->io_time_left--;
+            if (iter->io_time_left == 0) {
+                iter->state = READY;
+                ready_queue.push_back(*iter);
+                sync_queue(job_list, *iter);
+                
+                execution_status += print_exec_status(current_time, iter->PID, WAITING, READY);
+                iter = wait_queue.erase(iter);
+            }
+            else {
+                ++iter;
+            }
+        }
         /////////////////////////////////////////////////////////////////
 
         //////////////////////////SCHEDULER//////////////////////////////
-        FCFS(ready_queue); //example of FCFS is shown here
-        /////////////////////////////////////////////////////////////////
+        //Checking for running process
+        if (running.state == RUNNING){
+            running.remaining_time--;
+            quantum_remaining--;
+            cpu_time_since_last_io++;
 
+            //check if the process completed
+            if (running.remaining_time == 0){
+                execution_status += print_exec_status(current_time, running.PID, running.state, TERMINATED);
+                terminate_process(running, job_list);
+                idle_CPU(running);
+                quantum_remaining = TIME_QUANTUM;
+                cpu_time_since_last_io = 0;
+            }
+            
+            //check if the process needs to do I/O
+            else if(running.io_freq > 0 && cpu_time_since_last_io >= running.io_freq) {
+                // move to wait queue
+                running.state = WAITING;
+                running.io_time_left = running.io_duration;
+                wait_queue.push_back(running);
+                sync_queue(job_list, running);
+
+                execution_status += print_exec_status(current_time, running.PID, RUNNING, WAITING);
+                idle_CPU(running);
+                quantum_remaining = TIME_QUANTUM;
+                cpu_time_since_last_io = 0;
+            }
+
+            // check if the time queue expired (RR will preempt)
+            else if (quantum_remaining == 0){
+                running.state = READY;
+                ready_queue.insert(ready_queue.begin(), running);
+                sync_queue(job_list, running);
+
+                execution_status += print_exec_status(current_time, running.PID, RUNNING, READY);
+                idle_CPU(running);
+                cpu_time_since_last_io = 0;
+            }
+        }
+        /////////////////////////////////////////////////////////////////
+        if(running.state != RUNNING && !ready_queue.empty()) {
+            priority_sort(ready_queue);
+            running = ready_queue.back();
+            ready_queue.pop_back();
+
+            // only if the process is starting for the first time will the start time be set to the current time
+            // otherwise it retains its original start time
+            // This is to prevent overwriting the start time when a process is preempted and rescheduled
+            // run_process() function sets start time to current time unconditionally, meaning every time a process is scheduled
+            // its start time would be updated (even if it is just being rescheduled after preemption), which is incorrect.
+            if (running.start_time == -1) {
+                running.start_time = current_time;
+            }
+
+            states old_state = running.state;
+            running.state = RUNNING;
+            quantum_remaining = TIME_QUANTUM;
+            sync_queue(job_list, running);
+            execution_status += print_exec_status(current_time, running.PID, old_state, running.state);
+        }
+        current_time++;
     }
     
     //Close the output table
@@ -115,7 +199,7 @@ int main(int argc, char** argv) {
     //With the list of processes, run the simulation
     auto [exec] = run_simulation(list_process);
 
-    write_output(exec, "execution.txt");
+    write_output(exec, "execution_EP_RR.txt");
 
     return 0;
 }
